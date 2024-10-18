9cb4ca569a65fd1f114a1de45ad2e2eb6fc9744b
Shi Rao <github@homovetus.com>
2024-10-18 12:26:25 -0400

    edit

--- rtcp.c
+++ rtcp.c
@@ -1,4 +1,5 @@
 #include <glib.h>
+#include <gst/codecparsers/gsth264parser.h>
 #include <gst/gst.h>
 #include <gst/rtp/rtp.h>
 #include <gst/video/video.h>
@@ -26,6 +27,11 @@ void convert_ntp_to_datetime(guint64 ntp_timestamp) {
     unix_time.usec = (ntp_fraction * 1000000ULL) >> 32;
   }
 }
+
+// Emitted after a new manager (like rtpbin) was created and the default
+// properties were configured.
+static void new_manager_callback(GstElement *rtspsrc, GstElement *manager,
+                                 gpointer udata);
 
 /* Callback for receiving RTCP packets */
 static void on_receiving_rtcp_callback(GstPad *pad, GstPadProbeInfo *info,
@@ -96,17 +102,10 @@ static GstPadProbeReturn inject_sei(GstPad *pad, GstPadProbeInfo *info,
 }
 
 int main_func(int argc, char *argv[]) {
-  GstElement *pipeline;
-  GstElement *rtspsrc, *rtpjitterbuffer, *rtph264depay, *h264parse,
-      *splitmuxsink;
-  GstPad *depaysink_pad, *parsesrc_pad;
-  GstBus *bus;
-  GMainLoop *loop;
-
+  // Initialize the GStreamer library
   gst_init(&argc, &argv);
 
-  /* Create the pipeline */
-  pipeline =
+  GstElement *pipeline =
       gst_parse_launch("rtspsrc name=rtspsrc protocols=tcp "
                        "location=rtsp://user:pass@127.0.0.1:8554/stream0 ! "
                        "rtpjitterbuffer name=rtpjitterbuffer ! "
@@ -115,33 +114,88 @@ int main_func(int argc, char *argv[]) {
                        "splitmuxsink name=splitmuxsink location=video%02d.mp4 "
                        "max-size-time=10000000000 max-size-bytes=1000000",
                        NULL);
+  if (!pipeline) {
+    g_printerr("Pipeline can nat be created");
+    return -1;
+  }
 
-  loop = g_main_loop_new(NULL, FALSE);
+  // loop = g_main_loop_new(NULL, FALSE);
 
-  /* Connect RTSP manager callbacks */
-  rtspsrc = gst_bin_get_by_name(GST_BIN(pipeline), "rtspsrc");
-  rtph264depay = gst_bin_get_by_name(GST_BIN(pipeline), "rtph264depay");
-  depaysink_pad = gst_element_get_static_pad(rtph264depay, "sink");
-  h264parse = gst_bin_get_by_name(GST_BIN(pipeline), "h264parse");
-  parsesrc_pad = gst_element_get_static_pad(h264parse, "src");
+  GstElement *rtspsrc = gst_bin_get_by_name(GST_BIN(pipeline), "rtspsrc");
+  GstElement *rtph264depay =
+      gst_bin_get_by_name(GST_BIN(pipeline), "rtph264depay");
+  GstPad *depaysink_pad = gst_element_get_static_pad(rtph264depay, "sink");
+  GstElement *h264parse = gst_bin_get_by_name(GST_BIN(pipeline), "h264parse");
+  GstElement *parsesrc_pad = gst_element_get_static_pad(h264parse, "src");
 
   g_signal_connect(rtspsrc, "new-manager",
                    G_CALLBACK(on_receiving_rtcp_callback), NULL);
   gst_pad_add_probe(depaysink_pad, GST_PAD_PROBE_TYPE_BUFFER,
                     calculate_frame_timestamp, NULL, NULL);
-  gst_pad_add_probe(parsesrc_pad, GST_PAD_PROBE_TYPE_BUFFER, inject_sei, NULL,
-                    NULL);
+  // gst_pad_add_probe(parsesrc_pad, GST_PAD_PROBE_TYPE_BUFFER, inject_sei,
+  // NULL,
+  //                   NULL);
 
   /* Start the pipeline */
-  gst_element_set_state(pipeline, GST_STATE_PLAYING);
-  g_main_loop_run(loop);
+  GstStateChangeReturn ret = gst_element_set_state(pipeline, GST_STATE_PLAYING);
+  if (ret == GST_STATE_CHANGE_FAILURE) {
+    g_printerr("Unable to set the pipeline to the playing state\n");
+    gst_object_unref(pipeline);
+    return -1;
+  }
+  // g_main_loop_run(loop);
+
+  GstBus *bus = gst_element_get_bus(pipeline);
+  GstMessage *msg = gst_bus_timed_pop_filtered(
+      bus, GST_CLOCK_TIME_NONE, GST_MESSAGE_ERROR | GST_MESSAGE_EOS);
+  if (msg != NULL) {
+    GError *err;
+    gchar *debug_info;
+
+    switch (GST_MESSAGE_TYPE(msg)) {
+    case GST_MESSAGE_ERROR:
+      gst_message_parse_error(msg, &err, &debug_info);
+      g_printerr("Error received from element %s: %s\n",
+                 GST_OBJECT_NAME(msg->src), err->message);
+      g_printerr("Debugging information: %s\n",
+                 debug_info ? debug_info : "none");
+      g_clear_error(&err);
+      g_free(debug_info);
+      break;
+    case GST_MESSAGE_EOS:
+      g_print("End-Of-Stream reached.\n");
+      break;
+    default:
+      // Unreachable, only requested message types are ERROR and EOS
+      g_printerr("Unexpected message received.\n");
+      break;
+    }
+    gst_message_unref(msg);
+  }
 
   /* Cleanup */
+  gst_object_unref(bus);
   gst_element_set_state(pipeline, GST_STATE_NULL);
   gst_object_unref(pipeline);
-  g_main_loop_unref(loop);
-
+  // g_main_loop_unref(loop);
   return 0;
+}
+
+static void new_manager_callback(GstElement *rtspsrc, GstElement *manager,
+                                 gpointer udata) {
+  g_print("New manager created\n");
+  GstPad *rtcp_pad =
+      gst_element_request_pad_simple(manager, "recv_rtcp_sink_0");
+  if (!rtcp_pad) {
+    g_printerr("Failed to request recv_rtcp_sink_0 pad\n");
+    return;
+  }
+  RTPSession *session = g_signal_emit_by_name(manager, "get-internal-session");
+  if (!rtcp_pad) {
+    g_printerr("Failed to get internal session\n");
+    return;
+  }
+  g_signal_connect_after(instance, detailed_signal, c_handler, data)
 }
 
 int main(int argc, char *argv[]) {
