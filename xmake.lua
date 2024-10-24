b79690c4398708dcdbb76d6683208511c049c884
Shi Rao <github@homovetus.com>
2024-10-24 11:21:20 -0400

    removed unused option

--- xmake.lua
+++ xmake.lua
@@ -1,8 +1,6 @@
 add_rules("mode.debug", "mode.release")
 add_requires("pkgconfig::gstreamer-1.0")
 add_requires("pkgconfig::gstreamer-rtp-1.0")
--- add_requires("pkgconfig::gstreamer-plugins-base-1.0")
--- add_requires("pkgconfig::gstreamer-plugins-bad-1.0")
 
 target("console") do
     set_kind("binary")
@@ -10,8 +8,6 @@ target("console") do
     add_packages("pkgconfig::gstreamer-1.0")
     add_packages("pkgconfig::gstreamer-rtp-1.0")
     add_ldflags("-lgstcodecparsers-1.0", {force=true})
-    -- add_packages("pkgconfig::gstreamer-plugins-base-1.0")
-    -- add_packages("pkgconfig::gstreamer-plugins-bad-1.0")
     if is_mode("debug") then
         add_defines("DEBUG")
     end
