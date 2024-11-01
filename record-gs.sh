9710dec4ae5b075d2b55050006bc4611337f0dea
Shi Rao <github@homovetus.com>
2024-11-01 13:07:29 -0400

    new record script

--- record-gs.sh
+++ record-gs.sh
@@ -1,23 +1,17 @@
 #!/bin/bash
-
+input_url='rtsp://onvif:password!@192.168.0.136:554/cam/realmonitor?channel=1&subtype=0&unicast=true&proto=Onvif'
 args=(
-        # General
-        --eos-on-shutdown # Force EOS on sources before shutting the pipeline down
+    # General
+    --eos-on-shutdown # Force EOS on sources before shutting the pipeline down
 
-        # Pipeline
-        rtspsrc
-        location=rtsp://user:pass@127.0.0.1:8554/stream0
-        protocols=tcp
-        !
-        rtph264depay
-        !
-        h264parse
-        !
-        mp4mux
-        !
-        filesink
-        location=./camera.mp4
+    # Pipeline
+    rtspsrc location=$input_url protocols=tcp name=rtspsrc
+    rtspsrc. ! rtph264depay ! h264parse ! queue ! mux. # Video stream
+    rtspsrc. ! rtpmp4gdepay ! aacparse ! queue ! mux.audio_0 # Audio stream
+
+    # Muxing and output
+    splitmuxsink name=mux location=%s%%02d.mp4 max-size-time=300000000000 max-size-bytes=500000000 # 30 minutes, 500 MB
 )
 
-# start gstreamer to record rtsp stream
+# Start GStreamer to record RTSP stream
 gst-launch-1.0 "${args[@]}"
